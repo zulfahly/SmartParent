9b7d2ed6577f6b4f18314dd7201bef9da3f98749
zufahly <zulfahlym@gmail.com>
2017-05-02 00:03:27 +0700

    add permit activity

--- app/src/main/java/pnj/ti/b2013/smartparent/view/MainActivity.java
+++ app/src/main/java/pnj/ti/b2013/smartparent/view/MainActivity.java
@@ -86,7 +86,11 @@ public class MainActivity extends BaseActivity {
         perizinanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Bundle extras = new Bundle();
+                extras.putParcelable("student",student);
+
                 Intent intent = new Intent(MainActivity.this, PermitActivity.class);
+                intent.putExtras(extras);
                 startActivity(intent);
             }
         });
--- app/src/main/java/pnj/ti/b2013/smartparent/view/permit/PermitActivity.java
+++ app/src/main/java/pnj/ti/b2013/smartparent/view/permit/PermitActivity.java
@@ -1,12 +1,41 @@
 package pnj.ti.b2013.smartparent.view.permit;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import pnj.ti.b2013.smartparent.R;
+import pnj.ti.b2013.smartparent.model.Student;
+import pnj.ti.b2013.smartparent.service.VolleyTaskService;
 import pnj.ti.b2013.smartparent.view.BaseActivity;
+import pnj.ti.b2013.smartparent.view.MainActivity;
 
 public class PermitActivity extends BaseActivity {
+
+    private static final String CONSTANT_SAKIT = "S";
+    private static final String CONSTANT_IZIN = "I";
+
+    String TAG = PermitActivity.class.getSimpleName();
+
+    EditText studentName;
+    EditText className;
+    EditText permitDetail;
+    RadioGroup radioGroup;
+    RadioButton rbIzin;
+    RadioButton rbSakit;
+    Button submit;
+
+    String permitType;
+    Student student;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +46,77 @@ public class PermitActivity extends BaseActivity {
 
     private void initUI() {
         setToolbar("Notifikasi",true);
+        student = getIntent().getParcelableExtra("student");
+
+        studentName = (EditText) findViewById(R.id.studentName);
+        className = (EditText) findViewById(R.id.classOfStudent);
+        permitDetail = (EditText) findViewById(R.id.permitDetail);
+        radioGroup = (RadioGroup) findViewById(R.id.rgJenisIzin);
+        rbIzin = (RadioButton) findViewById(R.id.rbIzin);
+        rbSakit = (RadioButton) findViewById(R.id.rbSakit);
+        submit = (Button) findViewById(R.id.submitBtn);
+
+
+        studentName.setText(student.nama_siswa);
+        className.setText(student.nama_kelas);
+        studentName.setEnabled(false);
+        className.setEnabled(false);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rbIzin:
+                        permitType = String.valueOf(CONSTANT_IZIN);
+                        break;
+                    case R.id.rbSakit:
+                        permitType = String.valueOf(CONSTANT_SAKIT);
+                        break;
+                }
+
+            }
+        });
+        rbSakit.setChecked(true);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postPermitData();
+            }
+        });
 
     }
 
+    private void postPermitData() {
+        if (getTaskService().isNetworkAvailable()){
+            getTaskService().usePermit(studentName.getText().toString(),permitType,permitDetail.getText().toString());
+        }else {
+            Toast.makeText(getApplicationContext(),"Check Your Connection",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
     @Override
     public void receive(int type, boolean success, Bundle extras) {
+        if (success) {
+            String data = extras.getString(VolleyTaskService.RESPONSE_DATA);
+            switch (type) {
+                case VolleyTaskService.REQ_TYPE_ADD_IZIN:
+                    Toast.makeText(getApplicationContext(),"SUCCESS",Toast.LENGTH_LONG).show();
 
+                    extras = new Bundle();
+                    extras.putParcelable("student",student);
+
+                    Intent intent = new Intent(this, MainActivity.class);
+                    intent.putExtras(extras);
+                    startActivity(intent);
+                    break;
+            }
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.failed))
+                    .setMessage(extras.getString(VolleyTaskService.RESPONSE_MESSAGE))
+                    .show();
+        }
     }
 }
